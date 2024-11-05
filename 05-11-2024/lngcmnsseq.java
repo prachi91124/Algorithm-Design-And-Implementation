fac3b2765716fe389fb926fbe56beab020366903
Prachi Anand <prachianand321@gmail.com>
2024-11-05 11:52:26 +0530

    Modified code

--- 05-11-2024/lngcmnsseq.java
+++ 05-11-2024/lngcmnsseq.java
@@ -18,6 +18,7 @@ public class lngcmnsseq {
     static int LCShelper(String s1, String s2, int n1, int n2){
         if(n1 ==0 || n2 == 0) return 0;
 
+        if(dp1[n1][n2] != -1) return dp1[n1][n2];
         if(s1.charAt(n1-1) == s2.charAt(n2-1)){
             dp1[n1][n2] = 1 + LCShelper(s1, s2, n1-1, n2-1);
         }
